77a04854cdd159acd1106e6b99290fe4e5beddd0
Hideto KUROSAKI <50898673+H-Kurosaki@users.noreply.github.com>
2024-11-13 10:32:38 +0900

    Add files via upload

--- MyMCP3425.cpp
+++ MyMCP3425.cpp
@@ -7,13 +7,50 @@ MCP3425::MCP3425(TwoWire &w) {
 
 void MCP3425::begin(void) 
 {
-wire->beginTransmission(MCP3425_address);
-wire->write(MCP3425_configRegister);
+mode=16;
+	wire->beginTransmission(MCP3425_address);
+wire->write(MCP3425_configRegister16bit);
 wire->endTransmission();
 }
 
+
+void MCP3425::begin(int bitmode) 
+{
+mode=bitmode;
+
+	wire->beginTransmission(MCP3425_address);
+
+	if(mode==16)
+	{	
+	wire->write(MCP3425_configRegister16bit);
+	}
+	else if(mode==14)
+	{	
+	wire->write(MCP3425_configRegister14bit);
+	}
+	else
+	{
+	mode=12;
+	wire->write(MCP3425_configRegister12bit);
+	}
+
+	
+	wire->endTransmission();
+}
+
+
 double MCP3425::readVolt(void) {
-return (double)(readADC()) * MCP3425_VREF / 32767.0 ;
+	
+if(mode==16)
+	{
+	return (double)(readADC()) * MCP3425_VREF / 32767.0 ;
+	}
+	else if(mode==14)
+	{
+	return (double)(readADC()) * MCP3425_VREF / 8191.0 ;
+	}
+	
+	return (double)(readADC()) * MCP3425_VREF / 2047.0 ;	
 }
 
 int16_t MCP3425::readADC(void) {
--- MyMCP3425.h
+++ MyMCP3425.h
@@ -7,17 +7,20 @@
 
 #define MCP3425_address 0x68
 #define MCP3425_VREF    2.048
-#define MCP3425_configRegister 0b10011000 //16bit 15sps PGA x1 
+#define MCP3425_configRegister16bit 0b10011000 //16bit(15times/s) 15sps PGA x1 
+#define MCP3425_configRegister14bit 0b10010100 //14bit(60times/s) PGA x1 
+#define MCP3425_configRegister12bit 0b10010000 //12bit(240times/s) PGA x1 
 
 class MCP3425 {
   public:
   MCP3425(TwoWire &w);
 	void begin(void);
+	void begin(int bitmode);
 	double readVolt(void);
 	int16_t readADC(void);
   private:
 	TwoWire *wire;
-    
+    int mode;
 };
 
 #endif
